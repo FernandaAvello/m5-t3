d4cdc62c13a68c66d93d8cf64c03e957d0662096
fer <fernandaavellorocha@gmail.com>
2025-01-27 18:20:42 -0300

    agrego componentes anteriores

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Home from "../pages/Home";
@@ -8,21 +8,32 @@ import Services from "../pages/Services";
 import Appointment from "../pages/Appointment";
 import Patients from "../pages/Patients";
 import Login from "../pages/Login";
+import AccessDenied from "../pages/AccessDenied";
+import Vulnerabilities from "../pages/Vulnerabilities";
+import PrivateRoute from './PrivateRoute';
 
 const AppRoutes = () => {
+  const location = useLocation();
+  const isLoginPage = location.pathname === '/login';
 
   return (
     <>
-      <Header />
+      {!isLoginPage && <Header />}
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/vulnerabilities" element={<Vulnerabilities />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/team" element={<Team />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/appointment" element={<Appointment />} />
-        <Route path="/patients" element={<Patients />} />
+        <Route path="/access-denied" element={<AccessDenied />} />
+        <Route element={<PrivateRoute allowedRoles={['doctor', 'admin']} />}>
+          <Route path="/team" element={<Team />} />
+          <Route path="/services" element={<Services />} />
+        </Route>
+        <Route element={<PrivateRoute allowedRoles={['admin', 'doctor']} />}>
+          <Route path="/appointment" element={<Appointment />} />
+          <Route path="/patients" element={<Patients />} />
+        </Route>
       </Routes>
-      <Footer />
+      {!isLoginPage && <Footer />}
     </>
   );
 };
